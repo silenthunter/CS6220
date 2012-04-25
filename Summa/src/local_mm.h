491e15b263c77bacf7af428aeef4c7effeca692a
silenthunter <silenthunter@gmail.com>
2012-04-25 18:30:31 -0400

    CUDA now compiles, though nothing happens as of yet

--- Summa/src/local_mm.h
+++ Summa/src/local_mm.h
@@ -1,3 +1,6 @@
+#ifdef _nvcc
+extern "C" 
+#endif
 void local_mm (const int m, const int n, const int k, const double alpha,
 	       const double *A, const int lda, const double *B, const int ldb,
 	       const double beta, double *C, const int ldc);
