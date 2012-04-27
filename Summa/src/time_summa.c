5b347f195294db38c3dc0df3812add5e208840ea
silenthunter <silenthunter@gmail.com>
2012-04-27 11:06:10 -0400

    A decent candidate for benchmarks

--- Summa/src/time_summa.c
+++ Summa/src/time_summa.c
@@ -97,8 +97,8 @@ main (int argc, char *argv[])
   random_summa (512, 512, 512, 2, 2, 256, NUM_TRIALS);
   random_summa (1024, 1024, 1024, 2, 2, 512, NUM_TRIALS);
   random_summa (2048, 2048, 2048, 2, 2, 1024, NUM_TRIALS);
-  random_summa (4096, 4096, 4096, 2, 2, 512, NUM_TRIALS);
-  random_summa (8192, 8192, 8192, 2, 2, 128, NUM_TRIALS);
+  random_summa (4096, 4096, 4096, 2, 2, 1024, NUM_TRIALS);
+  random_summa (8192, 8192, 8192, 2, 2, 512, NUM_TRIALS);
 
 
   MPI_Finalize ();
