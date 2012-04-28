4f8ccb259891c607aefbe91e2d90904be7e32a49
silenthunter <silenthunter@gmail.com>
2012-04-28 01:00:17 -0400

    The correct time_summa.c file for the last commit

--- Summa/src/time_summa.c
+++ Summa/src/time_summa.c
@@ -17,6 +17,7 @@
 void
 random_summa (int m, int n, int k, int px, int py, int pb, int iterations)
 {
+  if(pb == 0) return;//Make sure we don't re-run pb=1 constantly
   int iter;
   double t_start, t_elapsed;
   int rank = 0;
@@ -78,6 +79,7 @@ main (int argc, char *argv[])
   int np = 0;
   char hostname[MPI_MAX_PROCESSOR_NAME + 1];
   int namelen = 0;
+  int scale = atoi(argv[1]);
 
   MPI_Init (&argc, &argv);	/* starts MPI */
   MPI_Comm_rank (MPI_COMM_WORLD, &rank);	/* Get process id */
@@ -91,12 +93,10 @@ main (int argc, char *argv[])
       printf ("Error: np=%d. Please use 4 processes\n", np);
     }
 
-  random_summa (256, 256, 256, 2, 2, 128, NUM_TRIALS);
-  random_summa (512, 512, 512, 2, 2, 256, NUM_TRIALS);
-  random_summa (1024, 1024, 1024, 2, 2, 512, NUM_TRIALS);
-  random_summa (2048, 2048, 2048, 2, 2, 512, NUM_TRIALS);
-  random_summa (4096, 4096, 4096, 2, 2, 512, NUM_TRIALS);
-  random_summa (8196, 8196, 8196, 2, 2, 512, NUM_TRIALS);
+  random_summa (256, 256, 256, 2, 2, 128 / scale, NUM_TRIALS);
+  random_summa (512, 512, 512, 2, 2, 256 / scale, NUM_TRIALS);
+  random_summa (1024, 1024, 1024, 2, 2, 512 / scale, NUM_TRIALS);
+  random_summa (2048, 2048, 2048, 2, 2, 1024 / scale, NUM_TRIALS);
 
 
   MPI_Finalize ();
